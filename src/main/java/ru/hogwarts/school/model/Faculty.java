365bdd07566130d60d3d24cdb1523dace2aec984
nusubbotin <nu.subbotin@ya.ru>
2022-12-13 08:10:16 +0300

    Правки чтобы приложение запустилось

--- src/main/java/ru/hogwarts/school/model/Faculty.java
+++ src/main/java/ru/hogwarts/school/model/Faculty.java
@@ -3,10 +3,8 @@ package ru.hogwarts.school.model;
 import javax.persistence.*;
 import java.util.Objects;
 import java.util.Set;
-
 @Entity
 public class Faculty {
-
     @Id
     @GeneratedValue
     private Long id;
@@ -16,8 +14,32 @@ public class Faculty {
     private String color;
 
     @OneToMany(cascade = CascadeType.ALL , fetch = FetchType.LAZY,
-            mappedBy = "faculty_id")
+            mappedBy = "faculty")
     private Set<Student> students;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 
     public Set<Student> getStudents() {
         return students;
@@ -26,26 +48,5 @@ public class Faculty {
     public void setStudents(Set<Student> students) {
         this.students = students;
     }
-
-    @Override
-    public String toString() {
-        return "Faculty{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", color='" + color + '\'' +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Faculty faculty = (Faculty) o;
-        return Objects.equals(id, faculty.id) && Objects.equals(name, faculty.name) && Objects.equals(color, faculty.color);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, color);
-    }
 }
+
